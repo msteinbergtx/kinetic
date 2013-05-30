3ed50c6ee1fde849680f6880730c32c1a19a4dde
Michael Steinberg <michaelsteinberg@Michaels-MacBook-Pro.local>
2013-05-29 20:16:50 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kinetic::Application.config.secret_token = '9181ef4adeb6fb9c6fd463f0ab9689c9132abdde664b781a2f682644fa3ebb05d7122b26d3f478079f16e1917f06a44fd45cbf6b27af7c226c2dad1c87c56966'
