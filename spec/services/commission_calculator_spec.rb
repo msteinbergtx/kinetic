a71d01da9bdb7ab8d8aea7ea963b5f148529e68b
Britton Ballard <davidbritton.ballard@gmail.com>
2013-07-03 00:16:46 -0700

    Added test coverage for commission calculator.

--- spec/services/commission_calculator_spec.rb
+++ spec/services/commission_calculator_spec.rb
@@ -33,6 +33,33 @@ describe CommissionCalculator do
       end
     end
 
+    it 'can handle just a number for the amount' do
+      rule = create(
+        :rule,
+        compensation_engine: create(
+          :basic_compensation,
+          calculation: "500"
+        )
+      )
+      organization = rule.organization
+      deal = create(
+        :deal,
+        name: 'for the test',
+        organization: organization,
+      )
+      create(
+        :commission_schedule,
+        deal: deal,
+        rule: rule,
+        commission_payment_date: Time.now + 1.day,
+        calculate_commission_date: Time.now - 1.day
+      )
+
+      CommissionCalculator.calculate(deal)
+
+      expect(deal.commissions.first.amount).to eq BigDecimal(500)
+    end
+
     it 'does not calculate if commissions already exist' do
       rule = create(
         :rule,
